21828e4009cce459268e5ad9ddd5fca7f7336807
KeoFoxy <arasdiel68@gmail.com>
2023-06-22 16:54:55 +0300

    Added specs

---
+++ Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  CarParadise
+//
+//  Created by Alexander Sorokin on 22.06.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TechnicalSpecs()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
