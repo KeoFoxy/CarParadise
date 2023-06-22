b173f793ad76658fae4efa08c7a3bae26f024732
KeoFoxy <arasdiel68@gmail.com>
2023-06-22 21:16:34 +0300

    CarView init

---
+++ Components/CarView.swift
@@ -0,0 +1,20 @@
+//
+//  CarView.swift
+//  CarParadise
+//
+//  Created by Alexander Sorokin on 22.06.2023.
+//
+
+import SwiftUI
+
+struct CarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarView()
+    }
+}
