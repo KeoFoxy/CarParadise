3ddb6b483ce6f988c9304a5c44b6a96c376664a6
KeoFoxy <arasdiel68@gmail.com>
2023-06-22 13:26:35 +0300

    Initial Commit

---
+++ CarParadise/CarParadiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarParadiseApp.swift
+//  CarParadise
+//
+//  Created by Alexander Sorokin on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarParadiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
