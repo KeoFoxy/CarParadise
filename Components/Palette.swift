db5f53e642555afdd1a5e1d4cbbba13474707b34
KeoFoxy <arasdiel68@gmail.com>
2023-06-25 14:32:33 +0300

    Palette

---
+++ Components/Palette.swift
@@ -0,0 +1,17 @@
+//
+//  Palette.swift
+//  CarParadise
+//
+//  Created by Alexander Sorokin on 25.06.2023.
+//
+
+import SwiftUI
+
+let topYellow: Color =  Color(hex: "#F5C652")
+let bottomYellow: Color = Color(hex: "#CF9C25")
+
+let mainGray: Color = Color(hex: "#F6F5F8")
+
+let bgYellow = LinearGradient(colors: [topYellow, bottomYellow],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
